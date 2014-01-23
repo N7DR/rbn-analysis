d18a4ef2efe49d2d960bbc065c95c6f7e24e0faa
N7DR <N7DR@arrl.net>
2014-01-23 14:34:54 -0700

    Add rank-sig.py

--- compare-sigs.py
+++ compare-sigs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
- 
-# Examples of data:
+
+# Examples:
 # KT1D-1,K,NA,3504.6,80m,NR4M,K,NA,CQ,35,2009-02-21 00:00:02+00,,,20090221,1235199602
 # WA7LNW,K,NA,28239,10m,AL7FS,KL,NA,DX,3,2013-12-12 23:59:59,12,CW,20131212,1386917999
 
@@ -28,11 +28,11 @@ import math
 import os
 import sys
 import time
-
+ 
 MAX_SNR_DIFFERENCE = 20    # restrict results to comparisons that are less than or equal to this valu
 MAX_TIME_DIFFERENCE = 600  # maximum difference in seconds for comparisons
 
-target_call = [sys.argv[1:2], sys.argv[2:3]]  # the two calls to be compared
+target_call = [ sys.argv[1], sys.argv[2] ]    # the two calls to be compared
 
 band = '20m'     # default
 continent = 'EU' # default
@@ -52,7 +52,7 @@ if len(sys.argv) > 5:
 
 if len(sys.argv) > 6:
   end_date = int(sys.argv[6]) 
-  
+
 # extract the lines with either of the target calls
 call_lines = [[] for x in range(2)]
 
@@ -73,7 +73,7 @@ for line in sys.stdin:
       short_line = [ fields[0], fields[2], fields[4], fields[5], int(fields[9]), int(fields[13]), int(fields[14]) ] 
       call_lines[n].append(short_line)
 
-# extract the lines with the target continent
+# extract the lines with the target continent       
 continent_lines = [[] for x in range(2)]
 
 for n in range(2):
@@ -85,6 +85,7 @@ for n in range(2):
 # extract the lines within the target date range
 # (could eliminate this in the case that the dates are the default values,
 # and copy continent_lines directly to date_lines))
+
 date_lines = [[] for x in range(2)]
 
 for n in range(2):
@@ -92,7 +93,7 @@ for n in range(2):
     this_date = short_line[5]
     if this_date >= start_date and this_date <= end_date:
       date_lines[n].append(short_line)
-
+      
 # accumulate all the differences in SNR
 # (we could just print them seriatim directly, but I'd rather accumulate first)
 sigs = []
@@ -107,7 +108,7 @@ for short_line_1 in date_lines[0]:
     spotter_2 = short_line_2[0]
     band_2 = short_line_2[2]
   
-    if spotter_1 == spotter_2 and abs(seconds_1 - seconds_2) <= time_diff and band_1 == band and band_2 == band:
+    if spotter_1 == spotter_2 and abs(seconds_1 - seconds_2) <= MAX_TIME_DIFFERENCE and band_1 == band and band_2 == band:
       snr_1 = short_line_1[4]
       snr_2 = short_line_2[4]
       difference = snr_2 - snr_1
@@ -115,13 +116,12 @@ for short_line_1 in date_lines[0]:
       if abs(difference) <= MAX_SNR_DIFFERENCE:
 	sigs.append(difference)
     
-    if seconds_2 > seconds_1 + time_diff:  # no need to check any more
+    if seconds_2 > (seconds_1 + MAX_TIME_DIFFERENCE):  # no need to check any more
       break
 
 # print all the differences in SNR
 for snr in sigs:
   print snr
-  
   
 
 
---
+++ rank-sig.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# Examples:
+# KT1D-1,K,NA,3504.6,80m,NR4M,K,NA,CQ,35,2009-02-21 00:00:02+00,,,20090221,1235199602
+# WA7LNW,K,NA,28239,10m,AL7FS,KL,NA,DX,3,2013-12-12 23:59:59,12,CW,20131212,1386917999
+
+# POSTER, POSTER-COUNTRY-PREFIX, POSTER-CONTINENT, FREQUENCY (kHz), BAND, DX, DX-COUNTRY-PREFIX, DX-CONTINENT, CQ, SNR (dB), DATE-AND-TIME, WPM, MODE, DATE, UNIX-EPOCH
+
+# 0: POSTER
+# 1: POSTER-COUNTRY-PREFIX
+# 2: POSTER-CONTINENT
+# 3: FREQUENCY (kHz)
+# 4: BAND
+# 5: DX
+# 6: DX-COUNTRY-PREFIX
+# 7: DX-CONTINENT
+# 8: CQ
+# 9: SNR (dB)
+# 10: DATE-AND-TIME
+# 11: WPM
+# 12: MODE
+# 13: DATE
+# 14: UNIX-EPOCH
+ 
+import datetime
+import math
+import os
+import sys
+import time
+ 
+# rank-sig CALL BAND CONTINENT START-DATE END-DATE
+
+data_filename='/zfs1/data/rbn/rbndata.csv'    # location of the RBN data
+ 
+target_call = sys.argv[1]
+
+band = '20m'     # default
+continent = 'EU' # default
+
+# dates may be either integers or strings
+start_date = '00000000'
+end_date   = '30000000'
+
+time_diff = 600
+
+if len(sys.argv) >= 2:
+  band = sys.argv[2]
+
+if len(sys.argv) >= 3:
+  continent = sys.argv[3]
+
+if len(sys.argv) >= 4:
+  start_date = sys.argv[4]
+
+if len(sys.argv) >= 5:
+  end_date = sys.argv[5]
+  
+call_lines = []
+
+# short_line:
+# 0: POSTER
+# 1: POSTER-CONTINENT
+# 2: BAND
+# 3: DX
+# 4: SNR (dB)
+# 5: DATE
+# 6: UNIX-EPOCH
+
+for line in sys.stdin:
+  fields = line.split(',')  
+  my_continent = fields[7]
+  short_line = [ fields[0], fields[2], fields[4], fields[5], int(fields[9]), fields[13], int(fields[14]) ] 
+  call_lines.append(short_line)
+
+poster_continent_lines = []
+
+for short_line in call_lines:
+  this_continent = short_line[1]
+  if this_continent == continent:
+    poster_continent_lines.append(short_line)
+
+date_lines = []
+
+for short_line in poster_continent_lines:
+  this_date = short_line[5]
+#  print this_date, start_date, end_date
+  if this_date >= start_date and this_date <= end_date:
+    date_lines.append(short_line)
+    
+band_lines = []
+
+for short_line in date_lines:
+  this_band = short_line[2]
+  if this_band == band:
+    band_lines.append(short_line)
+    
+if len(band_lines) == 0:
+  exit
+    
+my_lines = band_lines    # just to make it more obvious
+    
+#print my_lines
+#exit
+    
+# my_lines contains all the baseline QSOs
+
+# start by being really slow and simple
+
+results = {}  # key is epoch, value is a list of SNR measurements, of which the first is *my* signal at the poster
+
+with open(data_filename, 'r') as fp:    # handles opening and closing
+  
+  for line in fp:
+    fields = line.split(',')
+    
+    if fields[7] != my_continent:    # we are interested only in stations posted on the same continent
+      continue
+    
+    if fields[4] != band:
+      continue
+    
+    if fields[2] != continent:
+      continue
+    
+    if fields[5] == target_call:    # don't compare postings of me
+      continue
+    
+    rbn_time = int(fields[14])      # epoch seconds
+    remove_lines = []
+    
+    for my_line in my_lines:
+      
+      if not results.has_key(my_line[6]):
+	results[my_line[6]] = [ my_line[4] ]
+      
+      minimum_time = my_line[6] - time_diff
+      maximum_time = my_line[6] + time_diff
+      
+      if rbn_time > maximum_time:        # the datasets MUST be in chronological order
+        remove_lines.append(my_line)     # we will remove this line shortly
+        
+      if rbn_time >= minimum_time and rbn_time <= maximum_time:  # there is a match in time
+        my_poster = my_line[0]
+        rbn_poster = fields[0]
+        
+        if rbn_poster != my_poster:
+	  continue
+	
+        results[my_line[6]].append(int(fields[9]))    # appends in place, apparently; append the SNR from line 
+ 
+    for line_to_remove in remove_lines:    # now we can remove lines whose (epoch + time_diff) we have passed
+      my_lines.remove(line_to_remove)
+
+# dictionaries are unsorted
+for epoch, values in results.iteritems():
+  print epoch,
+  for value in values:
+    print value,
+  print
+
+
+  
+
+
